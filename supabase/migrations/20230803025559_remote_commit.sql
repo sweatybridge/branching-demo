a0db0f21d5aaa22aec9a79d7a35adb552da1366b
Qiao Han <qiao@supabase.io>
2025-05-21 12:58:35 +0800

    chore: remove pgsodium

--- supabase/migrations/20230803025559_remote_commit.sql
+++ supabase/migrations/20230803025559_remote_commit.sql
@@ -11,7 +11,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 SET pgaudit.log = 'none';
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+-- CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
 
 COMMENT ON SCHEMA "public" IS 'standard public schema';
 
