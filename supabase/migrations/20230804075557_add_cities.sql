sweatybridge/cli-demo

chore: add cities table

supabase/migrations/20230804075557_add_cities.sql
@@ -1,0 +1,5 @@
+create table cities (
+    id bigint generated by default as identity primary key,
+    name text not null,
+    country bigint references countries(id) on delete cascade
+)

sweatybridge/branching-demo

supabase/migrations/20230804075557_add_cities.sql
@@ -1,0 +1,5 @@
+create table cities (
+    id bigint generated by default as identity primary key,
+    name text not null,
+    country bigint references countries(id) on delete cascad
+)